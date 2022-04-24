1fedb61b7323e9d541e24c6cbd3b8d22deff2836
AzizNorboev <norboev6170@gmail.com>
2022-04-25 00:21:39 +0500

    Module7_DataStructures

---
+++ Module7/DataStructures/DataStructures/Tasks/HybridFlowProcessor.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using Tasks.DoNotChange;
+
+namespace Tasks
+{
+    public class HybridFlowProcessor<T> : IHybridFlowProcessor<T>
+    {
+        private readonly DoublyLinkedList<T> _linkedList = new DoublyLinkedList<T>();
+        public T Dequeue()
+        {
+            if (_linkedList.Length == 0)
+            {
+                throw new InvalidOperationException();
+            }
+            else
+            {
+                return _linkedList.RemoveAt(0);
+            }
+        }
+
+        public void Enqueue(T item)
+        {
+            _linkedList.Add(item);
+        }
+
+        public T Pop()
+        {
+            if (_linkedList.Length == 0)
+            {
+                throw new InvalidOperationException();
+            }
+            else
+            {
+                return _linkedList.RemoveAt(0);
+            }
+        }
+
+        public void Push(T item)
+        {
+            _linkedList.AddAt(0, item);
+        }
+    }
+}
