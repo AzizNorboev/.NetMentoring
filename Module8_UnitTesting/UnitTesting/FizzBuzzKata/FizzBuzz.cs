b864a02dcbeb5256fa0b1649a9e50d5dfba35c6d
AzizNorboev <norboev6170@gmail.com>
2022-05-02 19:18:05 +0500

    all tests are green now

---
+++ Module8_UnitTesting/UnitTesting/FizzBuzzKata/FizzBuzz.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FizzBuzzKata
+{
+    public class FizzBuzz
+    {
+        public string CheckForFizzBuzz(int number)
+        {
+            string result = string.Empty;
+
+            if (number < 1 || number > 100)
+            {
+                throw new InvalidOperationException();
+            }
+            else
+            {
+                if (number % 3 == 0 && number % 5 == 0)
+                {
+                    result = "FizzBuzz";
+                }
+                else if (number % 3 == 0)
+                {
+                    result = "Fizz";
+                }
+                else if (number % 5 == 0)
+                {
+                    result = "Buzz";
+                }
+                else
+                {
+                    result = number.ToString();
+                }
+            }
+
+            return result;
+        }
+    }
+}
