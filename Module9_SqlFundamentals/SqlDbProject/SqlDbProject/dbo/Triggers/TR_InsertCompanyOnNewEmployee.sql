da0dc773287db0371e709f2ac6f9655f24d7efe5
AzizNorboev <norboev6170@gmail.com>
2022-05-06 00:54:59 +0500

    All 4 tasks are completed

---
+++ Module9_SqlFundamentals/SqlDbProject/SqlDbProject/dbo/Triggers/TR_InsertCompanyOnNewEmployee.sql
@@ -0,0 +1,18 @@
+﻿-- TASK 4 TRIGGER
+
+CREATE TRIGGER Trg_InsertCompanyAfterNewEmployee
+ON Employee
+AFTER INSERT
+AS
+BEGIN
+    INSERT INTO Company(
+       Name,
+	   AddressId
+    )
+	SELECT
+       e.CompanyName,
+	   e.AddressId
+    FROM
+        Employee e where e.Id=@@IDENTITY
+   
+END
