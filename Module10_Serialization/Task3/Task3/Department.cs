ed05e7e8e104facdc2827b566921534f90924689
AzizNorboev <norboev6170@gmail.com>
2022-05-19 00:43:08 +0500

    All 3 tasks are completed now

---
+++ Module10_Serialization/Task3/Task3/Department.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task3
+{
+    [Serializable]
+    public class Department : ICloneable
+    {
+        public string DepartmentName { get; set; }
+        public List<Employee> Employees { get; set; }
+
+        public object Clone()
+        {
+            using (MemoryStream stream = new MemoryStream())
+            {
+                if (this.GetType().IsSerializable)
+                {
+                    BinaryFormatter formatter = new BinaryFormatter();
+                    formatter.Serialize(stream, this);
+                    stream.Position = 0;
+                    return formatter.Deserialize(stream);
+                }
+                return null;
+            }
+        }
+
+        public override string ToString()
+        {
+            return $"DepartmentName: {this.DepartmentName}" + Environment.NewLine +
+                $"Employees: {string.Join(", ", this.Employees)}";
+        }
+    }
+}
---
+++ Module10_Serialization/Task3/Task3/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Task3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Department department = new Department
+            {
+                DepartmentName = "IT",
+                Employees = new List<Employee>
+                {
+                 new Employee { EmployeeName = "Aziz" },
+                 new Employee { EmployeeName = "Petr" }
+                }           
+            };
+
+            Department clonedDepartment = (Department)department.Clone();
+
+            if(clonedDepartment != null)
+            {
+                Console.WriteLine(clonedDepartment.DepartmentName);
+                foreach(var item in clonedDepartment.Employees)
+                {
+                    Console.WriteLine(item.EmployeeName);
+                }
+            }
+            else
+            {
+                Console.WriteLine("clonedDepartment is null");
+            }
+            Console.ReadLine();
+        }
+    }
+}
