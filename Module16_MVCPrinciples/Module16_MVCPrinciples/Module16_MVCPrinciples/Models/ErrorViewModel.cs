d47361a48d07fd95ad6df4cc2e43c8d73fcb8bda
AzizNorboev <norboev6170@gmail.com>
2022-06-25 19:50:24 +0500

    MVC project created

---
+++ Module16_MVCPrinciples/Module16_MVCPrinciples/Module16_MVCPrinciples/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Module16_MVCPrinciples.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
